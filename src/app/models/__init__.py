46a8638fee377002a25d7537f37caafa630afe44
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-16 11:47:51 -0300

    feat(migrations): add Alembic configuration and initial migration for user table

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,6 +1,3 @@
-from .brand import Brand
-from .category import Category
-from .product import Product
 from .user import User
 
-__all__ = ['User', 'Product', 'Category', 'Brand']
+__all__ = ['User']
