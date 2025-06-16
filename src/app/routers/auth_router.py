12bc94de7b960f000a13f510d69ab3eab2af9465
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-16 11:39:53 -0300

    feat(routers): implement authentication endpoints for user login and retrieval

---
+++ src/app/routers/auth_router.py
@@ -0,0 +1,86 @@
+from http import HTTPStatus
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.session import get_async_session
+from app.models.user import User
+from app.routers.deps import get_current_user
+from app.schemas.response import Response
+from app.schemas.user.user_input_create import UserResponse
+from app.schemas.user.user_login_input import UserLoginInput, UserLoginResponse
+from app.services import auth_service
+
+Session = Annotated[AsyncSession, Depends(get_async_session)]
+router = APIRouter(prefix='/auth')
+
+
+@router.post(
+    '/login',
+    response_model=UserLoginResponse,
+    status_code=HTTPStatus.OK,
+    summary='User login',
+    description='This endpoint allows users to log in to the system using their email and password.',  # noqa: E501
+    responses={
+        HTTPStatus.OK.value: {
+            'model': UserLoginResponse,
+            'description': 'Login successful. Returns an access token.',
+        },
+        HTTPStatus.UNAUTHORIZED.value: {
+            'model': Response[Any],
+            'description': 'Invalid email or password.',
+        },
+        HTTPStatus.INTERNAL_SERVER_ERROR.value: {
+            'model': Response[Any],
+            'description': 'An unexpected error occurred. Please try again later.',  # noqa: E501
+        },
+        HTTPStatus.BAD_REQUEST.value: {
+            'model': Response[Any],
+            'description': 'Bad request. Please check the input data.',
+        },
+        HTTPStatus.UNPROCESSABLE_ENTITY.value: {
+            'model': Response[Any],
+            'description': 'Validation error. Please check the input data.',
+        },
+    },
+)
+async def user_login(
+    session: Session,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+) -> UserLoginResponse:
+    login_input = UserLoginInput(
+        email=form_data.username, password=form_data.password
+    )
+    return await auth_service.login(
+        login_input=login_input,
+        session=session,
+    )
+
+
+@router.get(
+    '/me',
+    response_model=Response[UserResponse],
+    status_code=HTTPStatus.OK,
+    summary='Get current user',
+    description='This endpoint retrieves the currently authenticated user.',
+    responses={
+        HTTPStatus.OK.value: {
+            'model': Response[UserResponse],
+            'description': 'User found successfully.',
+        },
+        HTTPStatus.UNAUTHORIZED.value: {
+            'model': Response[Any],
+            'description': 'User not authenticated.',
+        },
+    },
+)
+async def get_me(
+    current_user: User = Depends(get_current_user),
+) -> Response[UserResponse]:
+    return Response[UserResponse](
+        data=UserResponse.model_validate(current_user),
+        message='Usuário encontrado.',
+        status_code=HTTPStatus.OK.value,
+    )
