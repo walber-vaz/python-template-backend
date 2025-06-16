30078feecda355c008befe52da7d3b59aef70b65
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-16 11:39:01 -0300

    feat(models): add base model class for SQLAlchemy with UUID and timestamp fields

---
+++ src/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .brand import Brand
+from .category import Category
+from .product import Product
+from .user import User
+
+__all__ = ['User', 'Product', 'Category', 'Brand']
---
+++ src/app/models/base.py
@@ -0,0 +1,30 @@
+from uuid import UUID, uuid4
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class SQLAlchemyBase:
+    __abstract__ = True
+
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True,
+        default=uuid4,
+        unique=True,
+        nullable=False,
+        server_default=func.gen_random_uuid(),
+        init=False,
+    )
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        init=False,
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+        init=False,
+    )
