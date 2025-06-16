66f323b2a1e2f0c676e2105ac2531da709bc558c
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-16 11:39:07 -0300

    feat(models): create User model with fields for email, password, name, phone, and active status

---
+++ src/app/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import String, text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import table_registry
+from app.models.base import SQLAlchemyBase
+
+
+@table_registry.mapped_as_dataclass
+class User(SQLAlchemyBase):
+    __tablename__ = 'tb_users'
+
+    email: Mapped[str] = mapped_column(
+        String(200), unique=True, nullable=False
+    )
+    password: Mapped[str] = mapped_column(nullable=False)
+    first_name: Mapped[str] = mapped_column(String(80), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(80), nullable=False)
+    phone: Mapped[str] = mapped_column(String(15), nullable=True, unique=True)
+    is_active: Mapped[bool] = mapped_column(
+        nullable=False, server_default=text('TRUE'), default=True
+    )
