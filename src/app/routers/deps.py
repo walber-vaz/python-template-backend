a5848b05fb88e666b8f16aef6ee9116f8eaa6da2
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-23 23:03:54 -0300

    feat(routers): refactor dependency injection for user and auth services

--- src/app/routers/deps.py
+++ src/app/routers/deps.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+from uuid import UUID
+
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from jwt import DecodeError, decode
@@ -6,15 +9,52 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.config import settings
 from app.database.session import get_async_session
 from app.exceptions.user_exception import UserNotAuthenticated
-from app.repository import user_repository
+from app.repository.user import IUserRepositoryInterface, user_repository
+from app.security import (
+    create_access_token,
+    get_password_hash,
+    verify_password,
+)
+from app.services.auth import AuthService, IAuthServiceInterface
+from app.services.user import UserService
 
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl=f'{settings.API_PREFIX}/auth/login'
 )
+Session = Annotated[AsyncSession, Depends(get_async_session)]
+
+
+def get_user_repository(session: Session) -> IUserRepositoryInterface:
+    return user_repository.UserRepository(session=session)
+
+
+def get_user_service(
+    user_repository: Annotated[
+        IUserRepositoryInterface, Depends(get_user_repository)
+    ],
+) -> UserService:
+    return UserService(
+        user_repository=user_repository, password_hasher=get_password_hash
+    )
+
+
+def get_auth_service(
+    user_repository: Annotated[
+        IUserRepositoryInterface, Depends(get_user_repository)
+    ],
+) -> IAuthServiceInterface:
+    """Factory para serviço de autenticação"""
+    return AuthService(
+        user_repository=user_repository,
+        password_verifier=verify_password,
+        token_creator=create_access_token,
+    )
 
 
 async def get_current_user(
-    session: AsyncSession = Depends(get_async_session),
+    user_repository: Annotated[
+        IUserRepositoryInterface, Depends(get_user_repository)
+    ],
     token: str = Depends(oauth2_scheme),
 ):
     try:
@@ -28,15 +68,22 @@ async def get_current_user(
         sub_user_id = payload.get('sub')
 
         if not sub_user_id:
-            raise UserNotAuthenticated
-    except DecodeError:
-        raise UserNotAuthenticated
+            raise UserNotAuthenticated()
 
-    user = await user_repository.get_user_by_id(
-        user_id=sub_user_id, session=session
-    )
+        try:
+            user_id = (
+                UUID(sub_user_id)
+                if isinstance(sub_user_id, str)
+                else sub_user_id
+            )
+        except ValueError:
+            raise UserNotAuthenticated()
+    except DecodeError:
+        raise UserNotAuthenticated()
+
+    user = await user_repository.get_by_id(user_id)
 
     if not user:
-        raise UserNotAuthenticated
+        raise UserNotAuthenticated()
 
     return user
